82eddde18ce94b4c3b2ce38325fc31ef6afbab15
jonathan kalush <jonathan.kalush@gmail.com>
2026-02-27 16:39:24 +0200

    [minor] di

---
+++ TelemetryRecords/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+using MongoDB.Driver;
+using TelemetryRecords.Common.Constants;
+using TelemetryRecords.Models.Config;
+using TelemetryRecords.Repositories.AssignmentRepository;
+using TelemetryRecords.Repositories.AssignmentRepository.Interfaces;
+using TelemetryRecords.Services.AssignmentService;
+using TelemetryRecords.Services.AssignmentService.Interfaces;
+
+namespace TelemetryRecords.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWebApi(this IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.Converters.Add(
+                        new Newtonsoft.Json.Converters.StringEnumConverter()
+                    );
+                });
+            services.AddEndpointsApiExplorer();
+            return services;
+        }
+
+        public static IServiceCollection AddAppConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MongoDbConfiguration>(configuration.GetSection(TelemetryRecordsConstants.Configuration.MONGODB_CONFIG_SECTION));
+
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                MongoDbConfiguration config = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<MongoDbConfiguration>>().Value;
+                return new MongoClient(config.ConnectionString);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMongoDbServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAssignmentRepository, AssignmentRepository>();
+            services.AddScoped<IAssignmentService, AssignmentService>();
+            return services;
+        }
+    }
+}
