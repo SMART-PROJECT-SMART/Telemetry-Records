100e22b8df9475fa31ce45a053320b7778f612d9
jonathan kalush <jonathan.kalush@gmail.com>
2026-03-19 10:21:34 +0200

    [minorr] controler

--- TelemetryRecords/Controllers/AssignmentController.cs
+++ TelemetryRecords/Controllers/AssignmentController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using TelemetryRecords.Common.Constants;
 using TelemetryRecords.Models.Ro;
 using TelemetryRecords.Services.AssignmentService.Interfaces;
+using TelemetryRecords.Services.TelemetryDataService.Interfaces;
 
 namespace TelemetryRecords.Controllers
 {
@@ -10,10 +11,14 @@ namespace TelemetryRecords.Controllers
     public class AssignmentRecordsController : ControllerBase
     {
         private readonly IAssignmentService _assignmentService;
+        private readonly ITelemetryDataService _telemetryDataService;
 
-        public AssignmentRecordsController(IAssignmentService assignmentService)
+        public AssignmentRecordsController(
+            IAssignmentService assignmentService,
+            ITelemetryDataService telemetryDataService)
         {
             _assignmentService = assignmentService;
+            _telemetryDataService = telemetryDataService;
         }
 
         [HttpGet("latest")]
@@ -43,6 +48,25 @@ namespace TelemetryRecords.Controllers
             IEnumerable<AssignmentRo> assignments =
                 await _assignmentService.GetAssignmentsByDateAsync(parsedDate, cancellationToken);
             return Ok(assignments);
+        }
+
+        [HttpGet("telemetry")]
+        public async Task<ActionResult<List<MissionTelemetryRo>>> GetMissionTelemetry(
+            [FromQuery] string missionId,
+            [FromQuery] int tailId,
+            [FromQuery] DateTime startTime,
+            [FromQuery] DateTime endTime,
+            CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(missionId))
+            {
+                return BadRequest(TelemetryRecordsConstants.ErrorMessages.MISSION_ID_REQUIRED);
+            }
+
+            List<MissionTelemetryRo> telemetry = await _telemetryDataService.GetMissionTelemetryAsync(
+                missionId, tailId, startTime, endTime, cancellationToken);
+
+            return Ok(telemetry);
         }
     }
 }
